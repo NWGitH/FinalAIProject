4c7747bcb6f763e25091a6db05d902718a9c6cff
NWCode <nicholaswijaya711@gmail.com>
2024-03-02 19:25:07 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import urllib.request
+from fastai.vision.all import *
+
+def label_function(file_name):
+  if file_name[0].isupper():  # isupper is True if all the characters in the string are uppercase
+    return True
+  else:
+    return False
+
+# Load the pre-trained model
+model = load_learner('lion_vs_tiger.pkl')
+
+def predict(image):
+    img = PILImage.create(image)  # Use PILImage.create to open the image
+    pred_class, pred_idx, outputs = model.predict(img)
+    likelihood_is_lion = outputs[1].item()
+    if likelihood_is_lion > 0.9:
+        return "Lion"
+    elif likelihood_is_lion < 0.1:
+        return "Tiger"
+    else:
+        return "Not sure... try another picture!"
+
+# Streamlit app title and description
+st.title("Lion vs Tiger Classifier by Nicholas Wijaya")
+st.write("Upload an image, and I'll tell you whether it's a lion or a tiger!")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    # Display the uploaded image
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+    # Make predictions on the uploaded image
+    if st.button("Predict"):
+        prediction = predict(uploaded_file)
+        st.write(prediction)
+
+# Add a footer
+st.text("Built with Streamlit and Fastai")
